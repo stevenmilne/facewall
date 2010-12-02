6018e954c1dfe1eb8b8639c5515467175536ed13
Steven Milne <steven@digitaldelivery.co.uk>
2010-12-02 14:54:02 +0000

    remove initial confirmation msg

--- facewall.js
+++ facewall.js
@@ -1,23 +1,22 @@
-// a simple bounce / echo server which returns a string to all users
-// pair with facewall_client to create a twitter war wall
-//
-
+// a really simple bounce / echo server which returns a string to all users
 
 var sys = require("sys")
   , ws = require('../lib/ws/server');
+
+// ws is a great websocket server written in node we can piggyback onto
+// get it at https://github.com/miksago/node-websocket-server
 
 var server = ws.createServer({debug: true});
 
 // Handle WebSocket Requests
 server.addListener("connection", function(conn){
-  conn.send("Yay new connection: "+conn.id);
 
+  // normally we'd send something to confirm connection but we want to keep this clean
+  //conn.send("Yay new connection: "+conn.id);
+
+  // this is our main listener, which just broadcasts what it hears
   conn.addListener("message", function(message){
-  
-  //  conn.broadcast("<"+conn.id+"> "+message);
-  
     conn.broadcast(message);
-    
     if(message == "error"){
       conn.emit("error", "test");
     }
--- facewallmin.js
+++ facewallmin.js
@@ -2,8 +2,7 @@ var sys = require("sys"), ws = require('../lib/ws/server');
 
 var server = ws.createServer({debug: true});
 
-server.addListener("connection", function(conn){
-  conn.send("Yay new connection: "+conn.id);
+server.addListener("connection", function(conn){ 
   conn.addListener("message", function(message){
     conn.broadcast(message);
   });
